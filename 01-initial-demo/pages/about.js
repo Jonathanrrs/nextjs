a90b632208eee9bf80b3a3352553edf05ab5f9b7
jonathan <yoni_12rrs@hotmail.com>
2022-03-18 12:48:57 -0600

    Mi primera página

--- 01-initial-demo/pages/about.js
+++ 01-initial-demo/pages/about.js
@@ -1,18 +1,18 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function HomePage() {
+export default function AboutPage() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home -Jonathan</title>
+        <title>About -Jonathan</title>
         <meta name="description" content="Home Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <a href="/about">About</a>
+          Ir a <a href="/">Home</a>
         </h1>
 
         <p className={styles.description}>
