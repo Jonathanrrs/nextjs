65bab703017b6a11a6b47684619896f16dee45c9
jonathan <yoni_12rrs@hotmail.com>
2022-03-18 14:29:29 -0600

    Next/Link

--- 01-initial-demo/pages/about.js
+++ 01-initial-demo/pages/about.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function AboutPage() {
@@ -6,13 +7,14 @@ export default function AboutPage() {
     <div className={styles.container}>
       <Head>
         <title>About -Jonathan</title>
-        <meta name="description" content="Home Page" />
+        <meta name="description" content="About Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <a href="/">Home</a>
+          {/* Ir a <a href="/">Home</a> */}
+          Ir a <Link href="/">Home</Link>
         </h1>
 
         <p className={styles.description}>
--- 01-initial-demo/pages/index.jsx
+++ 01-initial-demo/pages/index.jsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import Link from 'next/link'
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
   return (
     <div className={styles.container}>
+      {/* Head componente de next, es la info que debería estar literalmente en el head del html */}
       <Head>
         <title>Home -Jonathan</title>
         <meta name="description" content="Home Page" />
@@ -12,7 +14,9 @@ export default function HomePage() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ir a <a href="/about">About</a>
+          {/* Ir a <a href="/about">About</a> */}
+          {/* esto es lo que se recomienda, hace un pre fetch de la página */}
+          Ir a <Link href="/about">About</Link>
         </h1>
 
         <p className={styles.description}>
