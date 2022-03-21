7ec6ed4a1776aa224f4f8da7012a7a4eebae160c
jonathan <yoni_12rrs@hotmail.com>
2022-03-21 13:19:27 -0600

    Multiples Layouts anidados

--- 01-initial-demo/pages/_app.js
+++ 01-initial-demo/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  /* es para utilizar nuestros layout anidado de about js */
+  const getLayout = Component.getLayout || ((page) => page);
+
+  // return <Component {...pageProps} />
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
--- 01-initial-demo/pages/about.js
+++ 01-initial-demo/pages/about.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
+import { DarkLayout } from "../components/layouts/DarkLayout";
 import { MainLayout } from "../components/layouts/MainLayout";
-
 
 export default function AboutPage() {
   return (
-    <MainLayout>
+    <>
       <h1>About Page</h1>
       <h1 className={"title"}>
         {/* Ir a <a href="/">Home</a> */}
@@ -14,6 +14,16 @@ export default function AboutPage() {
       <p className={"description"}>
         Get started by editing <code className={"code"}>pages/index.js</code>
       </p>
-    </MainLayout>
+    </>
   );
 }
+
+/* esto es para tener mas limpio nuestros layout anidados, es propio de next js, despues esto lo usamos en _app */
+
+AboutPage.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
+  );
+};
