b7e746cd46a6bbce7eff2555586c2c968c236e1f
jonathan <yoni_12rrs@hotmail.com>
2022-03-21 11:55:08 -0600

    Barra de navegación personalizada

--- 01-initial-demo/components/Navbar.js
+++ 01-initial-demo/components/Navbar.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+/* debemos importarlo asi, porque si es de manera global no permite */
+import styles from "./Navbar.module.css";
 export const Navbar = () => {
   return (
-    <nav>
-      <a href="/">Home</a>
-      <a href="/">About</a>
-      <a href="/">Contact</a>
+    /* se computa para poner estilos con un guion sino no es valido para js  */
+    <nav className={styles['menu-container']}>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
     </nav>
   );
 };
