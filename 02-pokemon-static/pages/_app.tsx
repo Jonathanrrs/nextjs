0e114485bffdf5ba0b33fc32353f6856aaa65748
jonathan <yoni_12rrs@hotmail.com>
2022-03-26 12:36:53 -0600

    Implementando el tema Dark

--- 02-pokemon-static/pages/_app.tsx
+++ 02-pokemon-static/pages/_app.tsx
@@ -1,13 +1,14 @@
-import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react';
-import '../styles/globals.css'
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { darkTheme } from "../themes";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
