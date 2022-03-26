a31af733dd3a199038a0d6e1dce85741d061c3c9
jonathan <yoni_12rrs@hotmail.com>
2022-03-26 12:18:14 -0600

    NextUI - React UI Library

--- 02-pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pages/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <Button color="gradient">Hola mundo</Button>
     </>
   );
 };
