16be90e49ace86f69f2b3e439f6fd172842c3b64
jonathan <yoni_12rrs@hotmail.com>
2022-04-05 21:26:58 -0500

    Generando las 151 páginas de Pokémons

--- 02-pokemon-static/pages/pokemon/[id].tsx
+++ 02-pokemon-static/pages/pokemon/[id].tsx
@@ -1,20 +1,16 @@
-import { GetStaticProps, NextPage,GetStaticPaths } from "next";
-import { useRouter } from "next/router";
+import { GetStaticProps, NextPage, GetStaticPaths } from "next";
+import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
+import { Pokemon } from "../../interfaces";
 
 interface Props {
-  // pokemon: any
-  id: string;
-  name: string;
+  pokemon: Pokemon
 }
 
-const PokemonPage: NextPage<Props> = ({id, name}) => {
-  const router = useRouter();
-  console.log(router.query);
-  
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
   return (
     <Layout title="Algun pokémon">
-      <h1>{id}-{name}</h1>
+      <h1>{pokemon.name}</h1>
     </Layout>
   );
 };
@@ -23,26 +19,36 @@ const PokemonPage: NextPage<Props> = ({id, name}) => {
 /* esto lo usamos porque tenemos la pagina con [id] */
 /* se ejecuta del lado del servidor, en buildtime */
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
-  return {
-    paths: [
-      {
-        params: {
-          id: '1'
-        }
-      }
-    ],
-    /* si se pone un url que no fue previamente renderizado regrese 404 */
-    fallback: false
-  }
-}
+  const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
+  console.log(pokemons151);
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  // const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
+  return {
+    // paths: [
+    //   {
+    //     params: {
+    //       id: "1",
+    //     },
+    //   },
+    // ],
+    /* esto viene a suplir lo de arriba de manera dinamica con los 151 pokemones */
+    paths: pokemons151.map((id) => ({
+      params: { id },
+    })),
+    /* si se pone un url que no fue previamente renderizado regrese 404 */
+    fallback: false,
+  };
+};
+
+/* aqui recibimos los params de arriba */
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  /* asi lo tipamos porque si lo hacemos de la mera manera son tipos raros y largos
+  y para nuestro uso asi resulta mejor como string */
+  const { id } = params as { id: string };
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
 
   return {
     props: {
-      id: '1',
-      name: 'Bulbasar'
+      pokemon: data,
     },
   };
 };
