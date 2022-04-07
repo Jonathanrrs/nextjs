804ab3b4f1195042d5ab07364911e94a34144b13
jonathan <yoni_12rrs@hotmail.com>
2022-04-07 14:07:42 -0500

    Pantalla de Favoritos

--- 02-pokemon-static/pages/favorites/index.tsx
+++ 02-pokemon-static/pages/favorites/index.tsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from "react";
 import { NextPage } from "next";
 import { Layout } from "../../components/layouts";
+import { NoFavorites } from "../../components/ui";
+import { localFavorites } from "../../utils";
 
 const FavoritesPage: NextPage = () => {
+  const [favoritePokemon, setFavoritePokemon] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemon(localFavorites.pokemons());
+  }, []);
+
   return (
     <Layout title="Favorites">
-      <h1>Favorites</h1>
+      <NoFavorites />
     </Layout>
   );
 };
