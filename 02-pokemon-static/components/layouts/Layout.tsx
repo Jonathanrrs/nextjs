67ce75945542168d3a51266f314b8989622ae377
jonathan <yoni_12rrs@hotmail.com>
2022-04-07 20:49:18 -0500

    Open Graph Meta Tags

--- 02-pokemon-static/components/layouts/Layout.tsx
+++ 02-pokemon-static/components/layouts/Layout.tsx
@@ -6,7 +6,10 @@ interface Props {
   children: JSX.Element | JSX.Element[];
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout = ({ children, title }: Props) => {
+ 
   return (
     <>
       <Head>
@@ -17,6 +20,16 @@ export const Layout = ({ children, title }: Props) => {
           content={`Información sobre el pokemon ${title}`}
         />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la página sobre ${title}`}
+        />
+        {/* se recomienda poner el path estatico, esto es para seo */}
+        <meta
+          property="og:image"
+          content={`${origin}/img/banner.png`}
+        />
       </Head>
       <Nabvar />
       <main style={{ padding: "0px 20px" }}>{children}</main>
--- 02-pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage, GetStaticProps } from "next";
-import { Grid } from "@nextui-org/react";
+import { Grid, Image } from "@nextui-org/react";
 import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
 import { PokemonListResponse, SmallPokemon } from "../interfaces";
