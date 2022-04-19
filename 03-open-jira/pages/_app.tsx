d3921e9a9ce7520b3633b839b7667c58e4ae8675
jonathan <yoni_12rrs@hotmail.com>
2022-04-19 14:44:35 -0500

    Instalación de Material-UI

--- 03-open-jira/pages/_app.tsx
+++ 03-open-jira/pages/_app.tsx
@@ -1,7 +1,19 @@
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
 
+const basicTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
