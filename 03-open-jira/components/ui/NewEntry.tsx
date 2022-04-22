1af527b66a5f8dc0a5e22034a0681b5c641665ab
jonathan <yoni_12rrs@hotmail.com>
2022-04-22 12:20:51 -0500

    Insertar entrada en el estado global

--- 03-open-jira/components/ui/NewEntry.tsx
+++ 03-open-jira/components/ui/NewEntry.tsx
@@ -1,12 +1,15 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useState, useContext } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { EntriesContext } from "../../context/entries";
 
 export const NewEntry = () => {
   const [isAdding, setIsAdding] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [touched, setTouched] = useState(false);
+
+  const { addNewEntry } = useContext(EntriesContext);
 
   const onTextFieldChanged = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -14,7 +17,11 @@ export const NewEntry = () => {
 
   const onSave = () => {
     if (inputValue.length === 0) return;
-    console.log(inputValue);
+    // console.log(inputValue);
+    addNewEntry(inputValue);
+    setIsAdding(false);
+    setTouched(false);
+    setInputValue("");
   };
 
   return (
--- 03-open-jira/context/entries/EntriesProvider.tsx
+++ 03-open-jira/context/entries/EntriesProvider.tsx
@@ -1,5 +1,5 @@
 import { FC, useReducer } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { EntriesContext, entriesReducer } from "./";
 import { Entry } from "../../interfaces";
 
@@ -15,20 +15,22 @@ const Entries_INITIAL_STATE: EntriesState = {
   entries: [
     {
       _id: uuidv4(),
-      description: 'Pendiente: Agregar los issues al proyecto final de React Native',
-      status: 'pending',
+      description:
+        "Pendiente: Agregar los issues al proyecto final de React Native",
+      status: "pending",
       createdAt: Date.now(),
     },
     {
       _id: uuidv4(),
-      description: 'En-progreso Crear un Pull Request al proyecto final de React Native',
-      status: 'in-progress',
+      description:
+        "En-progreso Crear un Pull Request al proyecto final de React Native",
+      status: "in-progress",
       createdAt: Date.now(),
     },
     {
       _id: uuidv4(),
-      description: 'Terminadas Agregar Redux al proyecto',
-      status: 'finished',
+      description: "Terminadas Agregar Redux al proyecto",
+      status: "finished",
       createdAt: Date.now(),
     },
   ],
@@ -37,10 +39,22 @@ const Entries_INITIAL_STATE: EntriesState = {
 export const EntriesProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
+  const addNewEntry = (description: string) => {
+    const newEntry: Entry = {
+      _id: uuidv4(),
+      description,
+      createdAt: Date.now(),
+      status: "pending",
+    };
+    dispatch({ type: "[Entry] - Add-Entry", payload: newEntry });
+  };
+
   return (
     <EntriesContext.Provider
       value={{
         ...state,
+        /* methods */
+        addNewEntry,
       }}
     >
       {children}
