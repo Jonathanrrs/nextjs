f90cc3fdd9bd06e66af4a7a2c11be2fb6427a56e
jonathan <yoni_12rrs@hotmail.com>
2022-04-23 18:29:55 -0500

    Leer las entradas desde el Backend

--- 03-open-jira/context/entries/entriesReducer.ts
+++ 03-open-jira/context/entries/entriesReducer.ts
@@ -1,9 +1,10 @@
 import { EntriesState } from "./";
 import { Entry } from "../../interfaces";
 
-type EntriesActionType = 
-| { type: "[Entry] - Add-Entry"; payload: Entry }
-| { type: "[Entry] - Entry-updated"; payload: Entry }
+type EntriesActionType =
+  | { type: "[Entry] - Add-Entry"; payload: Entry }
+  | { type: "[Entry] - Entry-updated"; payload: Entry }
+  | { type: "[Entry] - Refresh_Data"; payload: Entry[] };
 
 export const entriesReducer = (
   state: EntriesState,
@@ -13,19 +14,24 @@ export const entriesReducer = (
     case "[Entry] - Add-Entry":
       return {
         ...state,
-        entries: [...state.entries, action.payload]
+        entries: [...state.entries, action.payload],
       };
-    case '[Entry] - Entry-updated':
+    case "[Entry] - Entry-updated":
       return {
         ...state,
-        entries: state.entries.map(entry => {
-          if(entry._id === action.payload._id) {
+        entries: state.entries.map((entry) => {
+          if (entry._id === action.payload._id) {
             entry.status = action.payload.status;
             entry.description = action.payload.description;
           }
           return entry;
-        })
-      }
+        }),
+      };
+    case "[Entry] - Refresh_Data":
+      return {
+        ...state,
+        entries: [...action.payload],
+      };
 
     default:
       return state;
