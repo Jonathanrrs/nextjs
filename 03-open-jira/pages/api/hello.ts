0bd2cf0eeb3a6ff336c86b43c5bc8ad30532b36b
jonathan <yoni_12rrs@hotmail.com>
2022-04-23 13:12:12 -0500

    Conectarnos a MongoDB con Mongoose

--- 03-open-jira/pages/api/hello.ts
+++ 03-open-jira/pages/api/hello.ts
@@ -2,12 +2,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  ok: boolean;
+  msg: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ ok: true, msg: 'Correct' })
 }
