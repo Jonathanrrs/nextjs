4968ab50fd143da3d381250a49e858e53e7bf0e1
jonathan <yoni_12rrs@hotmail.com>
2022-04-25 14:49:11 -0500

    Terminar nuestro endpoint de actualización

--- 03-open-jira/pages/api/entries/[id].ts
+++ 03-open-jira/pages/api/entries/[id].ts
@@ -31,7 +31,9 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
   if (!entryToUpdate) {
     await db.disconnect();
-    return res.status(400).json({ message: "No hay entradas con ese ID " + id });
+    return res
+      .status(400)
+      .json({ message: "No hay entradas con ese ID " + id });
   }
 
   const {
@@ -39,12 +41,22 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     status = entryToUpdate.status,
   } = req.body;
 
-  const updatedEntry = await Entry.findByIdAndUpdate(
-    id,
-    { description, status },
-    /* para que revise el estado sea uno de los permitidos */
-    { runValidators: true, new: true }
-  );
+  /* esta es mas pesada porque se hace otra consulta */
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      { description, status },
+      /* para que revise el estado sea uno de los permitidos */
+      { runValidators: true, new: true }
+    );
+    res.status(200).json(updatedEntry!);
+  } catch (error: any) {
+    await db.disconnect();
+    res.status(400).json({message: error.errors.status.message})
+  }
 
-  res.status(200).json(updatedEntry!);
+  /* es lo mismo que lo de arriba */
+  // entryToUpdate.description = description;
+  // entryToUpdate.status = status;
+  // await entryToUpdate.save()
 };
