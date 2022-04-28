f11cd8b7c78aa19bc15028959c931c3f9501e878
jonathan <yoni_12rrs@hotmail.com>
2022-04-27 19:32:13 -0500

    Layout y Páginas adicionales

---
+++ 04-cookie-master/pages/about.tsx
@@ -0,0 +1,9 @@
+
+ const AboutPage = () => {
+  return (
+    <div>aboutPage</div>
+    <div>aboutPage</div>
+  )
+}
+
+export default AboutPage;
---
+++ 04-cookie-master/pages/theme-changer.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/layouts";
+
+export const ThemeChangerPage = () => {
+  return (
+    <Layout>
+      <h1>Theme changer</h1>
+    </Layout>
+  )
+};
+
+export default ThemeChangerPage;
