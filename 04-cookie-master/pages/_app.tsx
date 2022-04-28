eb94dfadb794efee195accf1cf736c96d3041761
jonathan <yoni_12rrs@hotmail.com>
2022-04-28 16:30:28 -0500

    Establecer el theme usando la cookie - Frontend

--- 04-cookie-master/pages/_app.tsx
+++ 04-cookie-master/pages/_app.tsx
@@ -1,19 +1,31 @@
+import { useEffect, useState } from "react";
 import "../styles/globals.css";
 import type { AppContext, AppProps } from "next/app";
 import { CssBaseline, Theme, ThemeProvider } from "@mui/material";
 import { darkTheme, lightTheme, customTheme } from "../themes";
+import Cookies from "js-cookie";
 
 interface Props extends AppProps {
   theme: string;
 }
 
 function MyApp({ Component, pageProps, theme }: Props) {
+  const [currentTheme, setCurrentTheme] = useState(lightTheme);
 
-  const currentTheme:Theme = theme === 'light'
-    ? lightTheme
-    : theme === 'dark'
-      ? darkTheme
-      : customTheme;
+  useEffect(() => {
+    /* el server no ejecuta lo que hay aqui dentro */
+
+    /* sino fuera por esto en un useefect habria posiblemente un error al obtener
+    el theme porque en el server y front serian diferentes */
+    const cookieTheme = Cookies.get("theme") || "light";
+    const selectedTheme: Theme =
+      cookieTheme === "light"
+        ? lightTheme
+        : cookieTheme === "dark"
+        ? darkTheme
+        : customTheme;
+    setCurrentTheme(selectedTheme);
+  }, []);
 
   return (
     <ThemeProvider theme={currentTheme}>
@@ -27,13 +39,13 @@ function MyApp({ Component, pageProps, theme }: Props) {
 /* getinitialProps no es recomendable usarlo, ademas nos convierte todas las paginas
 en server side */
 
-MyApp.getInitialProps = async (appContext: AppContext) => {
+// MyApp.getInitialProps = async (appContext: AppContext) => {
 
-  const {theme} = appContext.ctx.req ? (appContext.ctx.req as any).cookies : {theme: 'light'}
-  const validThemes = ["light", "dark", "custom"];
-  return {
-    theme: validThemes.includes(theme) ? theme : "dark",
-  }
-};
+//   const {theme} = appContext.ctx.req ? (appContext.ctx.req as any).cookies : {theme: 'light'}
+//   const validThemes = ["light", "dark", "custom"];
+//   return {
+//     theme: validThemes.includes(theme) ? theme : "dark",
+//   }
+// };
 
 export default MyApp;
