b67f36487ff1aae08ebea524db2008bf8b5ee493
jonathan <yoni_12rrs@hotmail.com>
2022-05-03 12:35:05 -0500

    Slideshow de las imágenes

--- 05-teslo-shop/pages/product/slug.tsx
+++ 05-teslo-shop/pages/product/slug.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import { ShopLayout } from "../../components/layouts/ShopLayout";
+import { ProductSlideShow } from "../../components/products";
 import { initialData } from "../../database/products";
 
 const product = initialData.products[0];
@@ -10,6 +11,7 @@ const ProductPage = () => {
       <Grid container spacing={3}>
         <Grid item xs={12} sm={7}>
           {/* slideshow */}
+          <ProductSlideShow images={product.images} />
         </Grid>
         <Grid item xs={12} sm={5}>
           <Box display="flex" flexDirection="column">
