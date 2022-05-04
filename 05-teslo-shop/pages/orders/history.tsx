c3e555c73fa0a66d73bdabd3bd7b9705917d000c
jonathan <yoni_12rrs@hotmail.com>
2022-05-03 22:11:33 -0500

    Historial de ordenes

---
+++ 05-teslo-shop/pages/orders/history.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Grid, Typography, Chip, Link } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { ShopLayout } from "../../components/layouts/ShopLayout";
+import NextLink from "next/link";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 100 },
+  { field: "fullname", headerName: "Nombre completo", width: 300 },
+
+  {
+    field: "paid",
+    headerName: "Pagada",
+    description: "Muestra información si está pagada la orden o no",
+    width: 200,
+    renderCell: (params: GridValueGetterParams) => {
+      return params.row.paid ? (
+        <Chip color="success" label="Pagada" variant="outlined" />
+      ) : (
+        <Chip color="error" label="No agada" variant="outlined" />
+      );
+    },
+  },
+  {
+    field: "orden",
+    headerName: "Ver orden",
+    width: 200,
+    sortable: false,
+    renderCell: (params: GridValueGetterParams) => {
+      return (
+        <NextLink href={`/orders/${params.row.id}`} passHref>
+          <Link underline="always">Orden</Link>
+        </NextLink>
+      );
+    },
+  },
+];
+
+const rows = [
+  { id: 1, paid: true, fullname: "Jonathan Ruiz" },
+  { id: 2, paid: false, fullname: "Sebastian Cordova" },
+  { id: 3, paid: false, fullname: "Alvaro Fidalgo" },
+  { id: 4, paid: true, fullname: "Carlos Mariscales" },
+];
+
+const HistoryPage = () => {
+  return (
+    <ShopLayout
+      title="Historial de ordenes"
+      pageDescription="Historial de ordenes del cliente"
+    >
+      <Typography variant="h1" component="h1">
+        Historial de ordenes
+      </Typography>
+      <Grid container>
+        <Grid item xs={12} sx={{ height: 650, width: "100%" }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+          />
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default HistoryPage;
