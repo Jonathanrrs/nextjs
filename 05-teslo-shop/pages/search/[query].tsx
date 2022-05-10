03e6e6a28e3b961506c603a6c71121fdd41369bd
jonathan <yoni_12rrs@hotmail.com>
2022-05-09 22:07:34 -0500

    Mejorar la experiencia de usuario en la búsqueda

--- 05-teslo-shop/pages/search/[query].tsx
+++ 05-teslo-shop/pages/search/[query].tsx
@@ -1,5 +1,5 @@
 import type { NextPage, GetServerSideProps } from "next";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { ShopLayout } from "../../components/layouts";
 import { ProductList } from "../../components/products";
 import { dbProducts } from "../../database";
@@ -7,9 +7,11 @@ import { IProduct } from "../../interfaces";
 
 interface Props {
   products: IProduct[];
+  foundProducts: boolean;
+  query: string;
 }
 
-const SearchPage: NextPage<Props> = ({ products }) => {
+const SearchPage: NextPage<Props> = ({ products, foundProducts, query }) => {
   return (
     <ShopLayout
       title={"Teslo-shop - Search"}
@@ -17,11 +19,23 @@ const SearchPage: NextPage<Props> = ({ products }) => {
     >
       {/* bueno para el seo el typography */}
       <Typography variant="h1" component="h1">
-        Buscar producto
+        Buscar productos
       </Typography>
-      <Typography variant="h2" sx={{ mb: 1 }}>
-        ABC ---123
-      </Typography>
+      {foundProducts ? (
+        <Typography variant="h2" sx={{ mb: 1 }}>
+          Término: {query}
+        </Typography>
+      ) : (
+        <Box display="flex">
+          <Typography variant="h2" sx={{ mb: 1 }}>
+            No encontramos ningún producto
+          </Typography>
+          <Typography variant="h2" sx={{ ml: 1 }} color="secondary">
+            {query}
+          </Typography>
+        </Box>
+      )}
+
       <ProductList products={products} />
     </ShopLayout>
   );
@@ -46,11 +60,17 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   }
 
   let products = await dbProducts.getProductsByTerm(query);
+  const foundProducts = products.length > 0;
   /* todo ,retornar otros productos */
+  if (!foundProducts) {
+    products = await dbProducts.getAllProducts();
+  }
 
   return {
     props: {
       products,
+      foundProducts,
+      query,
     },
   };
 };
