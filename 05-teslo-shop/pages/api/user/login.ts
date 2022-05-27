ab87d879e2bd1f00dd6a280018064f1863b9ef7f
jonathan <yoni_12rrs@hotmail.com>
2022-05-27 16:51:56 -0500

    Crear JWT - JsonWebToken

--- 05-teslo-shop/pages/api/user/login.ts
+++ 05-teslo-shop/pages/api/user/login.ts
@@ -2,17 +2,18 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcryptjs";
 import { db } from "../../../database";
 import { User } from "../../../models";
+import { jwt } from "../../../utils";
 
-type Data = 
-| { message: string;}
-| { 
-  token: string;
-  user: {
-    email: string;
-    name: string;
-    role: string;
-  }
-};
+type Data =
+  | { message: string }
+  | {
+      token: string;
+      user: {
+        email: string;
+        name: string;
+        role: string;
+      };
+    };
 
 export default function handler(
   req: NextApiRequest,
@@ -51,10 +52,12 @@ const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     });
   }
 
-  const { role, name } = user;
+  const { role, name, _id } = user;
+
+  const token = jwt.signToken(_id, email);
 
   return res.status(200).json({
-    token: "", /* jwt */
+    token, /* jwt */
     user: {
       email,
       role,
