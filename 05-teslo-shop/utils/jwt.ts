9103f815b4955c38d4c9cc6660b6b1ad95435b58
jonathan <yoni_12rrs@hotmail.com>
2022-05-27 21:05:54 -0500

    Validar y renovar JWT

--- 05-teslo-shop/utils/jwt.ts
+++ 05-teslo-shop/utils/jwt.ts
@@ -7,10 +7,28 @@ export const signToken = (_id: string, email: string) => {
 
   return jwt.sign(
     /* payload */
-    {_id, email},
+    { _id, email },
     /* seed */
     process.env.JWT_SECRET_SEED,
     /* opctions */
-    {expiresIn: '30d'}
-  )
+    { expiresIn: "30d" }
+  );
+};
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error("No hay semilla de JWT- Revisar variables de entorno");
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || "", (err, payload) => {
+        if (err) return reject("JWT no es válido");
+        const { _id } = payload as { _id: string };
+        resolve(_id);
+      });
+    } catch (error) {
+      reject('JWT no es válido')
+    }
+  });
 };
