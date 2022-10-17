1d7fbc93bea4c8879988088cc290f33615a749ac
jonathan <yoni_12rrs@hotmail.com>
2022-10-16 19:15:21 -0500

    Mostrar página de carrito vacío

--- 05-teslo-shop/pages/cart/index.tsx
+++ 05-teslo-shop/pages/cart/index.tsx
@@ -1,3 +1,5 @@
+import { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
 import {
   Box,
   Button,
@@ -9,7 +11,23 @@ import {
 } from "@mui/material";
 import { CartList, OrderSummary } from "../../components/cart";
 import { ShopLayout } from "../../components/layouts/ShopLayout";
+import { CartContext } from "../../context";
+
 const CartPage = () => {
+
+  const { isLoaded, cart } = useContext(CartContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded && cart.length === 0) {
+      router.replace("/cart/empty");
+    }
+  }, [isLoaded, cart, router]);
+
+  if(!isLoaded || cart.length === 0) {
+    return <></>
+  }
+
   return (
     <ShopLayout
       title="Carrito - 3"
@@ -20,7 +38,6 @@ const CartPage = () => {
       </Typography>
       <Grid container>
         <Grid item xs={12} sm={7}>
-          {/* carlist */}
           <CartList editable />
         </Grid>
         <Grid item xs={12} sm={5}>
@@ -28,7 +45,6 @@ const CartPage = () => {
             <CardContent>
               <Typography variant="h2">Orden</Typography>
               <Divider sx={{ my: 1 }} />
-              {/* order summary */}
               <OrderSummary />
               <Box sx={{ mt: 3 }}>
                 <Button color="secondary" className="circulat-btn">
