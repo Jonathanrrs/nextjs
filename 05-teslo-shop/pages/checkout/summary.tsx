ee55bbb3a10e93d9c8bd5d280dc42853a4db19f3
jonathan <yoni_12rrs@hotmail.com>
2022-10-17 11:47:09 -0500

    Mostrar la dirección en pantalla

--- 05-teslo-shop/pages/checkout/summary.tsx
+++ 05-teslo-shop/pages/checkout/summary.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import NextLink from "next/link";
 import {
   Box,
@@ -6,14 +7,40 @@ import {
   CardContent,
   Divider,
   Grid,
-  LinearProgress,
+  // LinearProgress,
   Link,
   Typography,
 } from "@mui/material";
 import { CartList, OrderSummary } from "../../components/cart";
 import { ShopLayout } from "../../components/layouts/ShopLayout";
+import { CartContext } from "../../context";
+import { countries } from "../../utils";
 
 const SummaryPage = () => {
+  const { shippingAddress, numberOfItems } = useContext(CartContext);
+
+  if (!shippingAddress) {
+    return <></>;
+  }
+
+  const {
+    firstName,
+    lastName,
+    address,
+    address2 = "",
+    city,
+    country,
+    phone,
+    zip,
+  } = shippingAddress;
+
+  const getCountryName = () => {
+    const countryFiltered = countries.filter(
+      (countryFilter) => countryFilter.code === country
+    );
+    return countryFiltered[0].name;
+  };
+
   return (
     <ShopLayout title="Resumen de orden" pageDescription="Resumen de la orden">
       <Typography variant="h1" component="h1">
@@ -27,20 +54,33 @@ const SummaryPage = () => {
         <Grid item xs={12} sm={5}>
           <Card className="summary-card">
             <CardContent>
-              <Typography variant="h2">Resumen (3 productos)</Typography>
+              <Typography variant="h2">
+                Resumen ({numberOfItems}{" "}
+                {numberOfItems === 1 ? "producto" : "productos"})
+              </Typography>
               <Divider sx={{ my: 1 }} />
               <Box display="flex" justifyContent="space-between">
-              <Typography variant="subtitle1">Dirección de entrega</Typography>
+                <Typography variant="subtitle1">
+                  Dirección de entrega
+                </Typography>
                 <NextLink href="/checkout/address" passHref>
                   <Link underline="always">Editar</Link>
                 </NextLink>
               </Box>
-             
-              <Typography>Jonathan Ruiz</Typography>
-              <Typography>232 Algún lugar</Typography>
-              <Typography>Stittsville, HYA 235</Typography>
-              <Typography>Canadá</Typography>
-              <Typography>+1 4342121</Typography>
+
+              <Typography>
+                {firstName} {lastName}
+              </Typography>
+              <Typography>
+                {address}
+                {address2 ? `, ${address2}` : ""}
+              </Typography>
+              <Typography>
+                {city}, {zip}
+              </Typography>
+              {/* <Typography>{country}</Typography> */}
+              <Typography>{getCountryName()}</Typography>
+              <Typography>{phone}</Typography>
               <Divider sx={{ my: 1 }} />
               <Box display="flex" justifyContent="end">
                 <NextLink href="/cart" passHref>
