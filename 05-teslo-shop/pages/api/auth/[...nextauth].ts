99c8bec136963d7e6d7739e8addeeba32638a140
jonathan <yoni_12rrs@hotmail.com>
2022-10-17 14:41:31 -0500

    NextAuth Callbacks

--- 05-teslo-shop/pages/api/auth/[...nextauth].ts
+++ 05-teslo-shop/pages/api/auth/[...nextauth].ts
@@ -1,33 +1,58 @@
 /* cualquier url que venga dentro de aqui pasa por aquí */
 
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
     }),
     // ...add more providers here
     Credentials({
-      name: 'Custom Login',
+      name: "Custom Login",
       credentials: {
-        email: {label: 'Correo', type: 'email', placeholder: 'correo@google.com'},
-        password: {label: 'Contraseña', type: 'password', placeholder: 'Contraseña'},
+        email: {
+          label: "Correo",
+          type: "email",
+          placeholder: "correo@google.com",
+        },
+        password: {
+          label: "Contraseña",
+          type: "password",
+          placeholder: "Contraseña",
+        },
       },
       async authorize(credentials) {
-        return null
-      }
-    })
+        return null;
+      },
+    }),
   ],
 
-  /* callbacks */
+  /* Callbacks */
   /* por defecto es wjt */
   callbacks: {
-    
-  }
+    async jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        switch (account.type) {
+          case "oauth":
+            break;  
+          case "credentials":
+            token.user = user;
+            break;
+        }
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;  
+      session.user = token.user as any;
+      return session;
+    },
+  },
 };
 export default NextAuth(authOptions);
