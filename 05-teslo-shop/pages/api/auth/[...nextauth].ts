bc6afab89a08ea96da81ae78425d55d46886e368
jonathan <yoni_12rrs@hotmail.com>
2022-10-17 13:35:26 -0500

    Proveedor - Credenciales

--- 05-teslo-shop/pages/api/auth/[...nextauth].ts
+++ 05-teslo-shop/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@
 
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -11,6 +12,22 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
     // ...add more providers here
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: {label: 'Correo', type: 'email', placeholder: 'correo@google.com'},
+        password: {label: 'Contraseña', type: 'password', placeholder: 'Contraseña'},
+      },
+      async authorize(credentials) {
+        return null
+      }
+    })
   ],
+
+  /* callbacks */
+  /* por defecto es wjt */
+  callbacks: {
+    
+  }
 };
 export default NextAuth(authOptions);
