9c9b2a960d10778e2a40740dedaef35aed44d15b
jonathan <yoni_12rrs@hotmail.com>
2022-10-17 16:18:42 -0500

    NextJs Middlewares con Next Auth

--- 05-teslo-shop/pages/checkout/_middleware.tsx
+++ 05-teslo-shop/pages/checkout/_middleware.tsx
@@ -1,21 +1,32 @@
+import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { jwt } from "../../utils";
+// import { jwt } from "../../utils";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const { token = "" } = req.cookies;
+export async function middleware(req: NextRequest | any, ev: NextFetchEvent) {
+  /* Ya no es necesario porque tenemos NextAuth */
+  // const { token = "" } = req.cookies;
 
-  /* esto es nativo de js */
-  // return new Response('No autorizado', {
-  //   status: 401
-  // })
+  // /* esto es nativo de js */
+  // // return new Response('No autorizado', {
+  // //   status: 401
+  // // })
 
-  try {
-    await jwt.isValidToken(token);
-    /* como de node/express */
-    /* si es válido pasamos a la siguiente página */
-    return NextResponse.next();
-  } catch (error) {
+  // try {
+  //   await jwt.isValidToken(token);
+  //   /* como de node/express */
+  //   /* si es válido pasamos a la siguiente página */
+  //   return NextResponse.next();
+  // } catch (error) {
+  //   const requestedPage = req.page.name;
+  //   return NextResponse.redirect(`/auth/login?p=${requestedPage}`);
+  // }
+
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!session) {
     const requestedPage = req.page.name;
-    return NextResponse.redirect(`/auth/login?p=${requestedPage}`);
+    return NextResponse.redirect(`/auth/login?p${requestedPage}`);
+  } else {
+    return NextResponse.next();
   }
 }
