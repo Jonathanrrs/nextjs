e957cfb690ac43d508bb1948bbc54fa68b5820e2
jonathan <yoni_12rrs@hotmail.com>
2022-11-03 21:41:49 -0600

    Crear orden - Backend Parte 2

--- 05-teslo-shop/pages/api/orders/index.ts
+++ 05-teslo-shop/pages/api/orders/index.ts
@@ -2,11 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { db } from "../../../database";
 import { IOrder } from "../../../interfaces";
-import { Product } from "../../../models";
+import { Product, Order } from "../../../models";
 
-type Data = {
-  message: string;
-};
+type Data =
+  | {
+      message: string;
+    }
+  | IOrder;
 
 export default function handler(
   req: NextApiRequest,
@@ -26,10 +28,7 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
   /* verificar que tengamos un usuario */
   const session: any = await getSession({ req });
-  {
-    !session;
-  }
-  {
+  if (!session) {
     return res
       .status(401)
       .json({ message: "Debe de estar autenticado para hacer esto" });
@@ -42,7 +41,7 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const subTotal = orderItems.reduce((prev, current) => {
       const currentPrice = dbProducts.find(
-        (prod) => prod._id === current._id
+        (prod) => prod.id === current._id
       )?.price;
 
       if (!currentPrice) {
@@ -51,7 +50,26 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
       return currentPrice * current.quantity + prev;
     }, 0);
-  } catch (error) {}
+
+    const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+    const backendTotal = subTotal * (taxRate + 1);
+
+    if (total !== backendTotal) {
+      throw new Error("El total no cuadra con el monto");
+    }
+
+    /* todo bien hasta este punto */
+    const userId = session.user._id;
+    const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+    await newOrder.save();
+    return res.status(201).json(newOrder);
+  } catch (error: any) {
+    await db.disconnect();
+    console.log(error);
+    res.status(400).json({
+      message: error.message || "Revise logs del servidor",
+    });
+  }
 
   return res.status(201).json(req.body);
 };
